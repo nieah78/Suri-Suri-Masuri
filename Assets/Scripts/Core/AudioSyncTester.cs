74c24440276cda428fdad127a9247795940dac34
nieah78 <nieah@hanyang.ac.kr>
2026-07-19 23:35:23 +0900

    feat : Add AudioSyncTester
    음원 BPM 확인 시스템 - AudioSyncTester 추가

---
+++ Assets/Scripts/Core/AudioSyncTester.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSyncTester : MonoBehaviour
+{
+    [Header("Test Settings")]
+    [SerializeField] private AudioClip testClip; // 테스트 음악 파일
+    [SerializeField] private float bpm = 102f;
+
+    [Header("Metronome Settings")]
+    [SerializeField] private AudioClip clickSound;
+    [SerializeField] private bool playMetronome = true;
+
+    private AudioSource audioSource;
+    private AudioSource metronomeSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        metronomeSource = gameObject.AddComponent<AudioSource>();
+        metronomeSource.clip = clickSound;
+        metronomeSource.volume = 0.5f;
+    }
+    
+    void Start()
+    {
+        RhythmManager.Instance.SetBPM(bpm);
+
+        if (testClip != null)
+        {
+            audioSource.clip = testClip;
+            audioSource.Play();
+        }
+
+        RhythmManager.Instance.OnBeat += HandleBeat;
+    }
+
+    void OnDestroy()
+    {
+        if (RhythmManager.Instance != null)
+            RhythmManager.Instance.OnBeat -= HandleBeat;
+    }
+
+    private void HandleBeat()
+    {
+        if (playMetronome && clickSound != null)
+        {
+            metronomeSource.Play();
+        }
+
+        if (audioSource.isPlaying)
+        {
+            float musicTime = audioSource.time;
+            double dspTime = AudioSettings.dspTime;
+            Debug.Log($"[SyncTest] Beat {RhythmManager.Instance.BeatCount} | 음악: {musicTime:F3}s | dsp: {dspTime:F3}s");
+        }
+    }
+
+    void Update()
+    {
+        if(Keyboard.current != null & Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            audioSource.Stop();
+            audioSource.Play();
+            RhythmManager.Instance.SetBPM(bpm);
+            Debug.Log("[SyncTest] 음원 재시작");
+        }
+    }
+}
